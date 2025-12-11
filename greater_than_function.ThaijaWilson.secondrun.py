3918e250e418501fab211af258185aea56fb612b
MrsGhost2627 <tbelldavila@ivytech.edu>
2025-12-11 06:13:25 -0800

    greater_than_function.ThaijaWilson

---
+++ greater_than_function.ThaijaWilson.secondrun.py
@@ -0,0 +1,34 @@
+
+# ------------------------------------------------------
+# Function Definition
+# ------------------------------------------------------
+def greater_than(x, y):
+    """
+    Returns True if x is greater than y.
+    Otherwise returns False.
+    """
+    if x > y:
+        return True
+    else:
+        return False
+
+# ------------------------------------------------------
+# Main Program
+# ------------------------------------------------------
+
+# Assign values to variables
+a = 10
+b = 6
+
+# Call the function and store the result
+result = greater_than(a, b)
+
+# Print the formatted output
+print(
+    "The statement " 
+    + str(a) 
+    + " is greater than " 
+    + str(b) 
+    + " is " 
+    + str(result)
+)
